ba9150ed749170c3f674e94d2f70c2a19f79cce0
Gaetan_Tubaro <tubaro.gaetan@gmail.com>
2022-01-19 12:00:03 +0100

    Update class pokemon

--- pokedex/src/app/models/pokemon.ts
+++ pokedex/src/app/models/pokemon.ts
@@ -1,15 +1,20 @@
+export type TypePokemon = 
+    "Feu" | "Eau" | "Plante" | "Electrik" | "Spectre" | "Normal" | "Poison" | "Sol" | "Roche" | "Acier" | "Combat" | "Psy" | "Vol" | "Insecte" | "Dragon" | "Fée" | "Ténèbres" | "Glace";
+
+
+
 export class Pokemon {
     nom : string;
     numero : number;
-    type1 : string;
-    type2? : string;
+    type1 : TypePokemon;
+    type2? : TypePokemon;
     description : string;
     cri? : string;
     evolution : number;
     taille : number ;
     poids : number;
 
-    constructor(nom : string, numero: number,type1: string,type2 : string, description: string, evolution: number, taille : number, poids : number){
+    constructor(nom : string, numero: number,type1: TypePokemon,type2 : TypePokemon, description: string, evolution: number, taille : number, poids : number){
         this.nom = nom;
         this.numero = numero;
         this.type1 = type1;
