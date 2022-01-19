10e2928f6bde65ea421bd1291a6a352b0262ad13
Shiryoa <yannis.bougourzi@gmail.com>
2022-01-19 16:18:53 +0100

    Formulaire upgrade

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListeComponent } from './liste/liste.component';
 import { FormulaireComponent } from './formulaire/formulaire.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormulaireComponent } from './formulaire/formulaire.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
