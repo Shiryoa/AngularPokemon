93fa1cbfbe899a7791b893c0d8779441babaf508
Shiryoa <yannis.bougourzi@gmail.com>
2022-01-19 14:13:18 +0100

    component

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeComponent } from './liste/liste.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeComponent,
+    FormulaireComponent
   ],
   imports: [
     BrowserModule,
