18b3687958000df6083f0452537e2665a559cd6e
fabien <fabienfarucci@yahoo.fr>
2022-01-19 16:45:30 +0100

    navigation

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListeComponent } from './liste/liste.component';
 import { FormulaireComponent } from './formulaire/formulaire.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListeComponent,
-    FormulaireComponent
+    FormulaireComponent,
+    Page1Component,
+    Page2Component,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
